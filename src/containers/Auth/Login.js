72ee50e3c59a3cc8fe2fdc80f6c622877812ca5a
Igorlave <masanoringuyen@gmail.com>
2023-06-22 16:32:11 +0700

    b34

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -9,8 +9,28 @@ import { FormattedMessage } from 'react-intl';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
     }
 
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleLogin = () => {
+        console.log(this.state.username)
+        console.log(this.state.password)
+    }
 
     render() {
        
@@ -21,14 +41,25 @@ class Login extends Component {
                         <div className='col-12 text-login'>Login</div>
                         <div className='col-12 form-group login-input'>
                             <label>Username</label>
-                            <input type='text' className='form-control' placeholder='Enter your username'></input>
+                            <input type='text' 
+                            className='form-control' 
+                            placeholder='Enter your username'
+                            value={this.state.username}
+                            onChange={(event) => this.handleOnChangeUsername(event)}
+                            ></input>
                         </div>
                         <div className='col-12 form-group login-input'>
                             <label>Password</label>
-                            <input type='password' className='form-control' placeholder='Enter your password'></input>
+                            <input type='password' 
+                            className='form-control' 
+                            placeholder='Enter your password'
+                            onChange={(event) => {this.handleOnChangePassword(event)}}
+                            ></input>
                         </div>
                         <div className='col-12 '>
-                            <button className='btn-login'>Login</button>
+                            <button className='btn-login' onClick = {() => {this.handleLogin()}}>
+                                Login
+                            </button>
                         </div>
                         <div className='col-12'>
                             <span className='forgot-pass'>
